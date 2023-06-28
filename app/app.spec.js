b6b16cd3dea1be85b25ce5aa7600157c4905d744
ramadhaniabidin12@gmail.com <ramadhaniabidin12@gmail.com>
2023-06-28 20:26:31 +0700

    update app.spec.js

--- app/app.spec.js
+++ app/app.spec.js
@@ -3,9 +3,9 @@
 describe('PhoneListController', function(){
 
     beforeEach(module('phonecatApp'));
-    it('should create a `phones` model with 3 phones', inject(function($contorller){
+    it('should create a `phones` model with 3 phones', inject(function($controller){
         var scope = {};
-        var ctrl = $contorller('PhoneListController', {$scope: scope});
+        var ctrl = $controller('PhoneListController', {$scope: scope});
 
         expect(scope.phones.length).toBe(3);
     }))
