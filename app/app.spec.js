863826537a4ee297062c9a987210f5457d35100f
ramadhaniabidin12@gmail.com <ramadhaniabidin12@gmail.com>
2023-06-28 21:09:42 +0700

    update app.js, app.spec.js, and index.html

--- app/app.spec.js
+++ app/app.spec.js
@@ -8,5 +8,6 @@ describe('PhoneListController', function(){
         var ctrl = $controller('PhoneListController', {$scope: scope});
 
         expect(scope.phones.length).toBe(3);
+        expect(scope.name).toBe("dunia");
     }))
 })
